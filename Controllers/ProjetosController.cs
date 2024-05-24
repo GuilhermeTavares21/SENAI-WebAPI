835cf021f73a6ddef4723c4d96eff98eb1f7733d
Guilherme Tavares <guitprates@gmail.com>
2024-05-24 12:46:29 -0300

    SENAI - WebAPI02 - Criação do CRUD

--- Controllers/ProjetosController.cs
+++ Controllers/ProjetosController.cs
@@ -1,3 +1,4 @@
+using Exo.WebApi.Models;
 using Exo.WebApi.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,5 +20,46 @@ namespace Exo.WebApi.Controllers
         {
             return Ok(_projetoRepository.Listar());
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            Projeto projeto = _projetoRepository.GetById(id);
+            if (projeto == null)
+            {
+                return NotFound();
+            }
+            return Ok(projeto);
+        }
+        [HttpPost]
+        public IActionResult Adicionar(Projeto model)
+        {
+            _projetoRepository.Adicionar(model);
+            return StatusCode(201);
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult Atualizar ( int id, Projeto model)
+        {
+            _projetoRepository.Atualizar(id, model);
+            return StatusCode(204);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar( int id )
+        {
+            try
+            {
+                _projetoRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err);
+            }
+
+        }
+
+
     }
 }
--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -16,5 +16,33 @@ namespace Exo.WebApi.Repositories
         {
             return _context.Projetos.ToList();
         }
+
+        public Projeto GetById(int ProjetoId)
+        {
+            return _context.Projetos.FirstOrDefault(item => item.Id == ProjetoId);
+        }
+        public void Adicionar(Projeto model)
+        {
+            _context.Projetos.Add(model);
+            _context.SaveChanges();
+        }
+        public void Atualizar(int id, Projeto model)
+        {
+            Projeto projeto = _context.Projetos.Find(id);
+            if(projeto != null)
+            {
+                projeto.Nome = model.Nome;
+                projeto.Area = model.Area;
+                projeto.Status = model.Status;
+            }
+            _context.Projetos.Update(projeto);
+            _context.SaveChanges();
+        }
+        public void Deletar (int id)
+        {
+            Projeto projeto = _context.Projetos.Find(id);
+            _context.Projetos.Remove(projeto);
+            _context.SaveChanges();
+        }
     }
 }
